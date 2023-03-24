28d0f02e26d045eba5ca7ab642524dc9cb60975f
Evgeniy Filippov <Zhekafilipp@mail.ru>
2023-03-24 15:07:30 +0300

    validation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,54 @@
 
 import * as bootstrap from 'bootstrap'; // eslint-disable-line no-unused-vars
 import './styles/styles.scss';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import { keyBy, has } from 'lodash';
 
-alert('Hello World!');
+const state = {
+  link: '',
+};
+
+const validLinks = [];
+
+const schema = yup.object().shape({
+  link: yup.string().required().url(),
+});
+
+const validate = (fields) => {
+  try {
+    schema.validateSync(fields, { abortEarly: false });
+    return {};
+  } catch (e) {
+    return keyBy(e.inner, 'path');
+  }
+};
+
+const inputText = document.querySelector('#url-input');
+const sendButton = document.querySelector('.btn-primary');
+
+const render = () => {
+  const validLink = !has(validate(state), 'link');
+  if (validLink && !validLinks.includes(state.link)) {
+    validLinks.push(state.link);
+    if (inputText.classList.contains('is-invalid')) {
+      inputText.classList.remove('is-invalid');
+    }
+    inputText.value = '';
+    inputText.focus();
+  } else if (!inputText.classList.contains('is-invalid')) {
+    inputText.classList.add('is-invalid');
+  }
+};
+
+const watchedState = onChange(state, render);
+
+sendButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const currentUrl = inputText.value;
+  if (watchedState.link === currentUrl) {
+    render();
+  } else {
+    watchedState.link = currentUrl;
+  }
+});
